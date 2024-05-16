a502586969f8050c4a6677ddfe6738540ab64b25
dip-212 <149631263+dip-212@users.noreply.github.com>
2024-05-17 03:18:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+document.querySelector("button").addEventListener("dblclick",function(){
+    alert("You just double-clicked there");
+})
+document.querySelector("button").addEventListener("mouseover",function(){
+    alert("You just moved in");
+})
+document.querySelector("button").addEventListener("mouseout",function(){
+    alert("You just moved out");
+})
+document.querySelector("button").addEventListener("click",function(){
+    alert("You just clicked there");
+})
